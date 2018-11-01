bbf7f221e34ba83cbf4013b375c547ee9e9a9903
kobepeng <pengkobe123@gmail.com>
2018-11-01 10:47:30 +0800

    feat(shared): init shared

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { PipesModule } from '@pipes/pipes.module';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+
+import { DebounceClickDirective } from '@directives/debounce-click.directive';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, 'assets/i18n/', '.json');
+}
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    PipesModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
+  declarations: [DebounceClickDirective],
+  providers: [], // better be empty!
+  exports: [
+    PipesModule,
+    TranslateModule,
+    DebounceClickDirective,
+    IonicModule,
+    CommonModule,
+    FormsModule,
+  ],
+})
+export class SharedModule {}
